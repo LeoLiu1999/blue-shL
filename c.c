9735203780f5cbf2644ff683d2eb7b1091d2f67e
Jake Zaia <jzaia@stuy.edu>
2017-12-03 20:37:15 -0500

    Commented up code

--- c.c
+++ c.c
@@ -11,28 +11,47 @@
 
 #define DEBUGGING 0 //Set to 1/0 as appropriate
 
-//returns the new fileno of stdin
-//changes STDIN_FILENO to target
-void redirect_stdin(char** chargs, int i){
+/**
+ * Changes STDIN_FILENO to target, where chargs[i+1] is the target
+ * Inputs:
+ *   chargs: All of the commands to be executed
+ *   i: The index of the character '<' in chargs
+ **/
+void redirect_stdin(char **chargs, int i){
   int inFile = open(chargs[i+1], O_RDONLY);
   dup2( inFile, STDIN_FILENO);
-  while (chargs[i]){
+  while (chargs[i]){ // Adjusts input as necessary
     chargs[i] = chargs[i+2];
     i++;}
 }
 
 
-void redirect_stdout(char** chargs, int i){
+/**
+ * Changes STDOUT_FILENO to target, where chargs[i+1] is the target
+ * Inputs:
+ *   chargs: All of the commands to be executed
+ *   i: The index of the character '>' in chargs
+ **/
+void redirect_stdout(char **chargs, int i){
   int outFile = open(chargs[i+1], O_CREAT | O_WRONLY, 0744);
   dup2( outFile, STDOUT_FILENO);
-  while (chargs[i]){
+  while (chargs[i]){ // Adjusts input as necessary
     chargs[i] = chargs[i+2];
     i++;}
 }
 
-char **parse_args(char * line, char *delimiter){
+
+/**
+ * Parses "line" as a command, separating on delimiter
+ * Inputs:
+ *   line: A line of user input
+ *   delimiter: The character to use for strsep
+ * Outputs:
+ *   An array of strings representing tokens of user input
+ **/
+char **parse_args(char *line, char *delimiter){
   char **ret = malloc(100);
-  ret[0]=line;
+  ret[0] = line;
   unsigned char i = 1;
   char * args;
   while( (args = strsep(&line, delimiter)))
@@ -41,6 +60,12 @@ char **parse_args(char * line, char *delimiter){
   return ret;
 }
 
+
+/**
+ * Handles shell builtins (cd & exit)
+ * Inputs:
+ *   chargs: All the commands to be executed
+ **/
 void special_funcs(char ** chargs) {
   char *cmd = chargs[0];
 
@@ -52,11 +77,16 @@ void special_funcs(char ** chargs) {
     exit(0); //sometimes doesn't work?
 }
 
+
+/**
+ * Executes a single bash command
+ * Inputs:
+ *   line: The command to be executed
+ **/
 void execute_single(char * line){
+  // handle strange inputs
   if (!line)
     return;
-
-  // Remove extra chars
   while (*line == ' ' || *line == '\n')
     line++;
 
@@ -65,23 +95,21 @@ void execute_single(char * line){
     return special_funcs(chargs);
 
   int f = fork(); // Fork off a child to exec commands
-
-  if (!f) {
+  if (!f) { // Child runs the code
     int i;
-    
-    i = 1; // scan for redirects
-    while (chargs[i]){
-      if(!strcmp(chargs[i],"<")) {
+
+    i = 1;
+    while (chargs[i]){ // Scan for redirects
+      if(!strcmp(chargs[i],"<")) { // Redirect STDIN
         if (DEBUGGING) printf("Redirecting input of \"%s\" from \"%s\"\n", chargs[i-1], chargs[i+1]);
-        redirect_stdin(chargs, i);
-        break;
+        redirect_stdin(chargs, i--);
       }
-      else if(!strcmp(chargs[i],">")) {
+      else if(!strcmp(chargs[i],">")) { // Redirect STDOUT
         if (DEBUGGING) printf("Redirecting output of \"%s\" to \"%s\"\n", chargs[i-1], chargs[i+1]);
-        redirect_stdout(chargs, i);
-        break;
+        redirect_stdout(chargs, i--);
       }
-      i++;
+      else
+        i++;
     }
 
     if (DEBUGGING) { // Print debug info
@@ -93,15 +121,16 @@ void execute_single(char * line){
       }
       printf("\n");
     }
-    
     execvp(chargs[0], chargs + 1);
   }
-
   wait(&f);
+  free(chargs); // USA! USA!
 }
 
+
+// Main wrapper for function execution, preps input to be executed
 void execute(char * line) {
-  if (line)
+  if (line) // Remove trailing newline, and ignore null cmds
     *strchr(line, '\n') = 0;
   else
     return;
@@ -117,19 +146,23 @@ void execute(char * line) {
     printf("\n\n");
   }
 
-  while (cmds[i++]){
-    //int fd = open("./foo", O_CREAT | O_RDWR, 0744);
-    //printf("FD: %d, ERRNO: %d\n", fd, errno);
+  while (cmds[i++])
     execute_single(cmds[i]);
-  }
 }
 
+/**
+ *  Prompts the user and stores their input in buf
+ * Inputs:
+ *   buf: The buffer for the command to be stored in
+ **/
 void prompt_user(char * buf) {
   char wd[100];
   printf("\e[36mblue-shL:\e[33m%s\e[37m$ ", getcwd(wd, 100));
   fgets(buf, 100, stdin);
 }
 
+
+// Main
 int main(){
   char *s = (char *)malloc(101);
   while ( 1 ){
